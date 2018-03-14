8a4b91360e798aeffcc3a33b4cb47ee32b61a086
David Ko <innobead@gmail.com>
2018-03-14 11:37:34 +0800

    Bug/fix virtualenv create error (#24)
    * Support Python 3 site-packages in environment variables
    
    * Commit 1.1.3
    
    * Fix pythonpath incorrect when installing virtualenv

--- src/main/kotlin/com/innobead/gradle/task/PythonRuntimeTask.kt
+++ src/main/kotlin/com/innobead/gradle/task/PythonRuntimeTask.kt
@@ -2,6 +2,7 @@ package com.innobead.gradle.task
 
 import com.innobead.gradle.GradleSupport
 import com.innobead.gradle.plugin.pythonPluginExtension
+import org.gradle.api.BuildCancelledException
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
 import java.io.File
@@ -30,14 +31,6 @@ class PythonRuntimeTask : DefaultTask() {
     fun action() {
         val commands = mutableListOf<String>()
 
-        listOf("2.7", "3.6").map { File(pythonDir, "lib/python$it/site-packages") }.find {
-            it.exists()
-        }?.apply {
-            commands.add(
-                    """export PYTHONPATH="${this.canonicalPath}":${'$'}PYTHONPATH"""
-            )
-        }
-
         commands.add("""export PATH="$pythonDir/bin":${'$'}PATH""")
 
         project.exec {
@@ -56,30 +49,56 @@ class PythonRuntimeTask : DefaultTask() {
             }
         }
 
-        project.exec {
+        val commandToCreateVirtualEnv = project.exec {
             it.isIgnoreExitValue = true
             it.executable("bash")
             it.args("-c", "which virtualenv > /dev/null 2>&1")
-        }.exitValue.also {
-            logger.lifecycle("Installing virtualenv")
-
+        }.exitValue.let {
             when (it) {
                 0 -> {
-                    commands.addAll(listOf(
-                            "virtualenv $virtualenvDir"
-                    ))
+                    "virtualenv $virtualenvDir"
                 }
                 else -> {
                     commands.addAll(listOf(
-                            "python -m pip install virtualenv -I --prefix $pythonDir $pipOptions".trim(),
-                            "$pythonDir/bin/virtualenv $virtualenvDir"
+                            "python -m pip install virtualenv -I --prefix $pythonDir $pipOptions".trim()
+
                     ))
+
+                    "$pythonDir/bin/virtualenv $virtualenvDir"
                 }
             }
         }
 
-        logger.lifecycle("Creating an environment")
+        logger.lifecycle("Installing virtualenv")
         logger.debug(commands.joinToString("\n"))
+
+        project.exec {
+            it.workingDir(project.extensions.pythonPluginExtension.tmpDir)
+            it.executable("bash")
+            it.environment(System.getenv())
+            it.args(listOf(
+                    "-c",
+                    commands.joinToString(";")
+            ))
+        }.rethrowFailure()
+
+        logger.lifecycle("Creating a virtual environment")
+
+        commands.clear()
+
+        listOf("2.7", "3.6").map { File(pythonDir, "lib/python$it/site-packages") }.find {
+            it.exists()
+        }?.apply {
+            commands.add(
+                    """export PYTHONPATH="${this.canonicalPath}":${'$'}PYTHONPATH"""
+            )
+        }
+
+        if (commands.isEmpty()) {
+            throw BuildCancelledException("No Python installed in $pythonDir")
+        }
+
+        commands.add(commandToCreateVirtualEnv)
 
         project.exec {
             it.workingDir(project.extensions.pythonPluginExtension.tmpDir)
